fa04d85b68f63c2f20f69391419aaeeacba23e90
rafidmax123 <mdrafiduddinprodhan@gmail.com>
2026-06-26 01:44:45 +0600

    creating with one problem I solved

---
+++ problem-two-sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+int main() {
+    double A,R;
+    cin>>R;
+   A = 3.14159*R*R;
+   cout<<fixed<<setprecision(4);
+    cout<<"A="<<A<<endl;
+    return 0;
+}
